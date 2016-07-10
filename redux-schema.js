10d646d52bfabb07725ecf3f47e10b24a831a3a6
atlas7 <johnnychan0302@gmail.com>
2016-07-10 22:57:07 +0100

    init commit basic infra hello world working

---
+++ redux-schema.js
@@ -0,0 +1,78 @@
+// Auth user only
+
+{
+  users: {
+    isAuthed,
+    isFetching,
+    error,
+    authedId,
+    [uid]: {
+      lastUpdated,
+      info: {
+        name,
+        uid,
+        avatar
+      }
+    }
+  },
+  modal: {
+    duck,
+    isOpen
+  },
+  ducks: {
+    isFetching,
+    error,
+    [duckId]: {
+      lastUpdated,
+      info: {
+        avatar,
+        duckId,
+        name,
+        text,
+        timestamp,
+        uid
+      }
+    }
+  },
+  usersDucks: {
+    isFetching,
+    error,
+    [uid]: {
+      lastUpdated,
+      duckIds: [duckId]
+    }
+  },
+  likeCount: {
+    [duckId]: 0
+  },
+  usersLikes: {
+    [duckId]: true
+  },
+  replies: {
+    isFetching,
+    error,
+    [duckId]: {
+      lastUpdated,
+      replies: {
+        [replyId]: {
+          name
+          comment
+          uid
+          timestamp
+          avatar
+        }
+      }
+    }
+  },
+  listener: {
+    [listenersId]: true
+  },
+  feed: {
+    isFetching,
+    error,
+    newDucksAvailable,
+    duckIdsToAdd: [duckId],
+    duckIds: [duckId]
+  }
+}
+
