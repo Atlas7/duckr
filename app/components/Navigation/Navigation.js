93b8a253bccdd7f82917d7c7d5cfd3c7abaff7c8
atlas7 <johnnychan0302@gmail.com>
2016-07-18 19:08:48 +0100

    add dynamic navLinks

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { container, navContainer, link } from './styles.css'
 
-export default function Navigation () {
+function NavLinks ({isAuthed}) {
+  return isAuthed === true ?
+    <ul>
+      <li><Link className={link} to='/'>{'Home'}</Link></li>
+    </ul>
+    :
+    null
+}
+
+function ActionLinks ({isAuthed}) {
+  return isAuthed === true ?
+    <ul>
+      <li>{'New Duck'}</li>
+      <li><Link className={link} to="/logout">{'logout'}</Link></li>
+    </ul>
+    :
+    <ul>
+      <li><Link className={link} to='/'>{'Home'}</Link></li>
+      <li><Link className={link} to="/auth">{'Authenticate'}</Link></li>
+    </ul>
+}
+
+
+export default function Navigation ({isAuthed}) {
   return (
-    <div>
-      Navigation
+    <div className={container}>
+      <nav className={navContainer}>
+        <NavLinks isAuthed={isAuthed} />
+        <ActionLinks isAuthed={isAuthed} />
+      </nav>
     </div>
   )
 }
