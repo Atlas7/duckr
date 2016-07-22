c0a65802e02415063c4947a039852bfb87e106a0
atlas7 <johnnychan0302@gmail.com>
2016-07-22 13:31:09 +0100

    add react-redux Provider

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import users from 'redux/modules/users'
 
 const store = createStore(users)
 console.log(store)
 console.log(store.getState())
 
-
-
-ReactDOM.render(routes, document.getElementById('app'))
+ReactDOM.render(
+  <Provider store={store}>
+    {routes}
+  </Provider>,
+  document.getElementById('app')
+)
