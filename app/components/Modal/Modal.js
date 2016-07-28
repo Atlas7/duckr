b92946e628f1d21efbb7f7b3e9f2689672135e68
atlas7 <johnnychan0302@gmail.com>
2016-07-28 16:09:32 +0100

    add Modal skeleton

---
+++ app/components/Modal/Modal.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react'
+
+const Modal = (props) => {
+  return (
+    <div>{'Modal'}</div>
+  )
+}
+
+Modal.propTypes = {
+}
+
+export default Modal
---
+++ app/containers/Modal/ModalContainer.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+import { Modal } from 'components'
+
+const ModalContainer = React.createClass({
+  propTypes: {
+  },
+  render () {
+    return (
+      <Modal />
+    )
+  },
+})
+
+export default ModalContainer
