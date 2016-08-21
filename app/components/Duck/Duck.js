96841f217e99e468693cfe9f4c6487fcc5191c53
atlas7 <johnnychan0302@gmail.com>
2016-08-21 09:14:40 +0100

    convert ducks state to immutable object

--- app/components/Duck/Duck.js
+++ app/components/Duck/Duck.js
@@ -2,30 +2,23 @@ import React, { PropTypes } from 'react'
 import { formatTimestamp } from 'helpers/utils'
 import Reply from 'react-icons/lib/fa/mail-reply'
 import Star from 'react-icons/lib/fa/star'
+import { Map } from 'immutable'
 
 import {
   duckContainer, contentContainer, avatar, actionContainer,
   header, text, likeReplyContainer, icon, likedIcon, author,
 } from './styles.css'
 
-const { func, object, bool, number, string, shape } = PropTypes
 Duck.propTypes = {
-  duck: shape({
-    avatar: string.isRequired,
-    duckId: string.isRequired,
-    name: string.isRequired,
-    text: string.isRequired,
-    timestamp: number.isRequired,
-    uid: string.isRequired,
-  }),
-  onClick: func,
-  isLiked: bool.isRequired,
-  addAndHandleLike: func.isRequired,
-  handleDeleteLike: func.isRequired,
-  numberOfLikes: number,
-  hideReplyBtn: bool.isRequired,
-  hideLikeCount: bool.isRequired,
-  goToProfile: func.isRequired,
+  duck: PropTypes.instanceOf(Map),
+  onClick: PropTypes.func,
+  isLiked: PropTypes.bool.isRequired,
+  addAndHandleLike: PropTypes.func.isRequired,
+  handleDeleteLike: PropTypes.func.isRequired,
+  numberOfLikes: PropTypes.number,
+  hideReplyBtn: PropTypes.bool.isRequired,
+  hideLikeCount: PropTypes.bool.isRequired,
+  goToProfile: PropTypes.func.isRequired,
 }
 
 export default function Duck (props) {
@@ -39,23 +32,23 @@ export default function Duck (props) {
       onClick={props.onClick}
     >
 
-      <img src={props.duck.avatar} className={avatar} />
+      <img src={props.duck.get('avatar')} className={avatar} />
 
       <div className={contentContainer}>
 
         <div className={header}>
-          <div onClick={props.goToProfile} className={author}>{props.duck.name}</div>
-          <div>{formatTimestamp(props.duck.timestamp)}</div>
+          <div onClick={props.goToProfile} className={author}>{props.duck.get('name')}</div>
+          <div>{formatTimestamp(props.duck.get('timestamp'))}</div>
         </div>
 
-        <div className={text}>{props.duck.text}</div>
+        <div className={text}>{props.duck.get('text')}</div>
 
         <div className={likeReplyContainer}>
           {props.hideReplyBtn === true
             ? null
             : <Reply className={icon} />}
           <div className={actionContainer}>
-            <Star className={starIcon} onClick={(e) => starFn(props.duck.duckId, e)} />
+            <Star className={starIcon} onClick={(e) => starFn(props.duck.get('duckId'), e)} />
             {props.hideLikeCount === true ? null : <div>{props.numberOfLikes}</div>}
           </div>
         </div>
